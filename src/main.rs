978cdd4eb26e78ea60b487c30d4c44678afa88a2
Josh Wong <lolpro11lolx@gmail.com>
2024-10-07 04:31:57 -0700

    typo

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,7 @@ async fn makedb(client: &Client) {
             agency_phone text NULL,
             agency_fare_url text NULL,
             agency_email text NULL,
-            onestop_feed_id text NOT NULL
+            onestop_feed_id text NOT NULL,
             PRIMARY KEY (onestop_feed_id, agency_id)
         );
     ").await.unwrap();
