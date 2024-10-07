1754cdd76554776aeae6688346367d242e980c5d
Josh Wong <lolpro11lolx@gmail.com>
2024-10-07 11:47:28 -0700

    add func

--- src/martin.rs
+++ src/martin.rs
@@ -13,6 +13,28 @@ async fn main() {
 
     client.batch_execute("
         CREATE OR REPLACE
+        FUNCTION shapes(z integer, x integer, y integer)
+            RETURNS bytea AS $$
+        DECLARE
+        mvt bytea;
+        BEGIN
+        SELECT INTO mvt ST_AsMVT(tile, 'shapes', 4096, 'geom') FROM (
+            SELECT
+            ST_AsMVTGeom(
+                ST_Transform(ST_CurveToLine(geom), 3857),
+                ST_TileEnvelope(z, x, y),
+                4096, 64, true) AS geom
+            FROM shapes
+            WHERE geom && ST_Transform(ST_TileEnvelope(z, x, y), 4326)
+        ) as tile WHERE geom IS NOT NULL;
+
+        RETURN mvt;
+        END
+        $$ LANGUAGE plpgsql IMMUTABLE STRICT PARALLEL SAFE;
+    ").await.unwrap();
+
+    client.batch_execute("
+        CREATE OR REPLACE
         FUNCTION busonly(z integer, x integer, y integer)
         RETURNS bytea AS $$
         DECLARE
